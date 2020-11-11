51bd29383cb0e8d15c914db4dae81d2dd9a9d462
rabia yılmaz <45205525+rabiayilmazz@users.noreply.github.com>
2020-11-12 00:00:28 +0300

    Constructor örneği
    temel anlamda constructor örnğidir. para çekme ve para yatırma işlemi yapar.

---
+++ Constructor1/src/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rabia
+ */
+public class Test {
+    public static void main(String[] args){
+       // Account account1 = new Account();
+        Account account2 = new Account("1234", 1000.0, "rabia yilmaz","yyilmazrabiaa@gmail.com","234566");
+        account2.paraYatir(5000);
+        account2.paraCekme(1500);
+    }
+}
